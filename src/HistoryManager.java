edf50d1ae406f3810df11f101f4980375a46624a
BEDOLAGA-rt <arseniyprokofev17279@gmail.com>
2025-04-28 20:59:57 +0300

    Update HistoryManager.java

--- src/HistoryManager.java
+++ src/HistoryManager.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 public interface HistoryManager {
-    public void add(Task task);
-    public void remove(int id);
-    public List<Task> getHistory();
+     void add(Task task);
+    void remove(int id);
+   List<Task> getHistory();
 }
